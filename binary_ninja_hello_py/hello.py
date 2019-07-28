7e9c052abe9616f8507daab042c9b2e1961cb54c
Andrew Lamoureux <andrew@vector35.com>
2019-07-28 12:09:03 -0400

    binary ninja "hello world" plugin for python

---
+++ binary_ninja_hello_py/hello.py
@@ -0,0 +1,9 @@
+# Despite [1] and [2], you don't have to choose any special names or make directories.
+# At the time of writing, any python files in the plugin folder get executed. It's up
+# to you to register yourself as a plugin.
+#
+# 1. https://docs.binary.ninja/guide/plugins/index.html
+# 2. https://github.com/Vector35/sample_plugin
+#
+#
+print("Hello, world! Python!")
