4effe59d6398c1da0f241e3bae3e8868e08e8bc6
Andrew Lamoureux <andrew@vector35.com>
2022-08-03 12:40:04 -0400

    Update hello_android_a64_gas

--- hello_android_a64_gas/hello.s
+++ hello_android_a64_gas/hello.s
@@ -2,17 +2,23 @@
 // kernel call: syscall number in x8, args in x0, x1, x2, ...
 // syscall number lookup <kernel-source-dir>/include/uapi/asm-generic/unistd.h 
 // 
-.global	_start
+.global	_start, _test
 
 .text
 
 _start:
-	mov		x2, msglen		// arg2: message length
+	bl		_test
+
+	ldr		x1, =msg0
+
+	mov		x2, msg0_len		// arg2: message length
 
 	// TODO: explore in detail why this won't work
 	//mov		x1, msg			// arg1: message
-	adrp	x1, msg
-	add		x1, x1, :lo12:msg
+
+	// we can also access the string by getting the 4k page and adding the offset
+	//adrp	x1, msg0
+	//add		x1, x1, :lo12:msg0
 
 	mov		x0, #1			// arg0: stdout
 	
@@ -23,9 +29,19 @@ _start:
 	mov		x8, #94			// __NR_exit
 	svc		#0
 
+_test:
+	// test Binja instrinsic _CountLeadingZeros()
+	clz		x1, x0
+	ldr		x0, =0xDEADBEEF
+	ret
+
 .data
 
-msg:
+msg0:
 	.asciz "Hello, world!\n"
+msg0_len = . - msg0
 
-msglen = . - msg
+msg1:
+	.asciz "I'm in test()\n"
+msg1_len = . - msg1
+
