c0572f6eb49650ae23c0e51acd3af4ed5df5c1a3
Andrew Lamoureux <andrew@vector35.com>
2024-03-02 01:02:48 -0500

    Commit before laptop upgrade

--- hello_uefi/main.c
+++ hello_uefi/main.c
@@ -5,13 +5,10 @@ typedef void *efi_handle_t;
 efi_status_t efi_main(
 	efi_handle_t handle, struct efi_system_table *system_table)
 {
+	__asm__("here: jmp here");
+
 	uint16_t msg[] = u"Hello World!";
 	efi_status_t status;
-
-	__builtin_debugtrap();
-	__builtin_debugtrap();
-	__builtin_debugtrap();
-	__builtin_debugtrap();
 
 	status = system_table->ConOut->clear_screen(system_table->ConOut);
 	if (status != 0)
